e022ba81756488de50cb7d41b2752120a9e11547
timeofwise <shockingpark@gmail.com>
2021-10-31 11:58:28 +0900

    initial

--- home/views.py
+++ home/views.py
@@ -1,15 +1,18 @@
 from django.shortcuts import render
 from .models import *
+import datetime
 
 # Create your views here.
 def home(request):
     template = "home/main.html"
     accounts = account.objects.all()
-    deposit_1 = deposit.objects.filter(deposit_account_id=1) #삼성증권
+    deposit_1 = deposit.objects.filter(deposit_account_id=1).filter(
+        date__gte=datetime.date(2020, 7, 1),
+        date__lte=datetime.date(2020, 7, 31)
+    ) #삼성증권
     deposit_2 = deposit.objects.filter(deposit_account_id=2)  # 국민은행 인덱스펀드
     deposit_3 = deposit.objects.filter(deposit_account_id=3)  # KB증권 미국주식 메인
     deposit_4 = deposit.objects.filter(deposit_account_id=4)  # 신한금투 미국주식 서브
-    len_accounts = len(accounts)
 
 
     context = {
