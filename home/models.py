3801895d42f2bb2fd1651114ac3f5d4983b5c164
timeofwise <shockingpark@gmail.com>
2021-10-31 14:24:38 +0900

    initial

--- home/models.py
+++ home/models.py
@@ -13,7 +13,7 @@ class account(models.Model):
 class deposit(models.Model):
     index = models.CharField(max_length=10, null=True, default="입금")
     amount = models.IntegerField(null=True, default=0)
-    deposit_account = models.ForeignKey(account, on_delete=models.PROTECT, default=3)
+    deposit_account = models.ForeignKey(account, on_delete=models.PROTECT, default=4)
 
     created = models.DateTimeField(null=True)
 
