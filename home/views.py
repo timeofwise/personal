53b2fa29ce58b0c482721d6a8f69aa9891a96fe2
timeofwise <shockingpark@gmail.com>
2021-10-31 11:31:55 +0900

    initial

--- home/views.py
+++ home/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
+from .models import *
 
 # Create your views here.
 def home(request):
     template = "home/main.html"
-    context = {}
+    accounts = account.objects.all()
+
+
+    context = {"accounts":accounts}
 
     return render(request, template, context)
