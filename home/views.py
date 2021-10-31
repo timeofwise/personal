69d0c439e0e4e4ac511a6276e18016e9d3f40a79
timeofwise <shockingpark@gmail.com>
2021-10-31 20:59:17 +0900

    initial

--- home/views.py
+++ home/views.py
@@ -76,8 +76,8 @@ def home(request):
                     else:
                         dict_asset[d.strftime("%Y-%m-%d")] = list_asset_temp[0]
             dummy.append(dict_asset)
-            dummy.append(dict_asset[today.strftime("%Y-%m-%d")])
-            total_asset_sum += dict_asset[today.strftime("%Y-%m-%d")]
+            dummy.append(dict_asset[endDate])
+            total_asset_sum += dict_asset[endDate]
 
         else:
             savings = deposit.objects.filter(deposit_account__ordering_level=i)
