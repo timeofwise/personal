0a7e08ce3b4db93f998bbb9d53bf5a9c6571faab
timeofwise <shockingpark@gmail.com>
2021-10-31 11:37:22 +0900

    initial

--- home/views.py
+++ home/views.py
@@ -5,9 +5,13 @@ from .models import *
 def home(request):
     template = "home/main.html"
     accounts = account.objects.all()
+    deposit_1 = deposit.objects.filter(deposit_account_id=1) #삼성증권
     len_accounts = len(accounts)
 
 
-    context = {"accounts":len_accounts}
+    context = {
+        "accounts":len_accounts,
+        "deposit_1":deposit_1,
+   }
 
     return render(request, template, context)
