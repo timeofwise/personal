53327ce2435be3eb590614e9341052bed2de99e8
timeofwise <shockingpark@gmail.com>
2021-10-31 12:02:00 +0900

    initial

--- home/views.py
+++ home/views.py
@@ -6,9 +6,7 @@ import datetime
 def home(request):
     template = "home/main.html"
     accounts = account.objects.all()
-    deposit_1 = deposit.objects.filter(deposit_account_id=1).filter(
-        created__gte=datetime.date(2020, 7, 1),
-        created__lte=datetime.date(2020, 7, 31)
+    deposit_1 = deposit.objects.filter(deposit_account_id=1).filter(created__range=["2020-07-01", "2020-07-31"])
     ) #삼성증권
     deposit_2 = deposit.objects.filter(deposit_account_id=2)  # 국민은행 인덱스펀드
     deposit_3 = deposit.objects.filter(deposit_account_id=3)  # KB증권 미국주식 메인
