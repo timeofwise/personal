fa1465de60b49fb670cd43e0d82c3d4420e1d4be
timeofwise <shockingpark@gmail.com>
2021-10-31 11:41:53 +0900

    initial

--- home/views.py
+++ home/views.py
@@ -6,12 +6,18 @@ def home(request):
     template = "home/main.html"
     accounts = account.objects.all()
     deposit_1 = deposit.objects.filter(deposit_account_id=1) #삼성증권
+    deposit_2 = deposit.objects.filter(deposit_account_id=2)  # 국민은행 인덱스펀드
+    deposit_3 = deposit.objects.filter(deposit_account_id=3)  # KB증권 미국주식 메인
+    deposit_4 = deposit.objects.filter(deposit_account_id=4)  # 신한금투 미국주식 서브
     len_accounts = len(accounts)
 
 
     context = {
         "accounts":len_accounts,
         "deposit_1":deposit_1,
+        "deposit_3":deposit_2,
+        "deposit_4":deposit_3,
+        "deposit_5":deposit_4,
    }
 
     return render(request, template, context)
