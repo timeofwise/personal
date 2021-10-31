e366cad9883edbc7b153579b36c624f98e0bacf6
timeofwise <shockingpark@gmail.com>
2021-10-31 17:50:33 +0900

    initial

--- home/models.py
+++ home/models.py
@@ -6,9 +6,13 @@ class account(models.Model):
     account_name = models.CharField(max_length=40, null=True)
     account_img = models.ImageField(upload_to='img/', default='img/no_image.png', null=True)
     description = models.CharField(max_length=100, null=True, default="-")
+    ordering_level = models.IntegerField(null=True)
 
     def __str__(self):
         return self.account_name
+
+    class Meta:
+        ordering = ['ordering_level']
 
 class deposit(models.Model):
     index = models.CharField(max_length=10, null=True, default="입금")
