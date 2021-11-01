0293e864c8536b3f2975d2148d8de6e5e204472f
timeofwise <shockingpark@gmail.com>
2021-11-01 21:18:01 +0900

    initial

--- config/settings.py
+++ config/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'mathfilters',
 
     'home',
+    'account',
 ]
 
 MIDDLEWARE = [
--- config/urls.py
+++ config/urls.py
@@ -21,5 +21,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('', include('home.urls')),
+    path('account/', include('acount.urls')),
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
