cd80b20656d7997718fb49db65610761d8e77552
timeofwise <shockingpark@gmail.com>
2021-11-01 21:18:43 +0900

    initial

--- config/urls.py
+++ config/urls.py
@@ -21,6 +21,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('', include('home.urls')),
-    path('account/', include('acount.urls')),
+    path('account/', include('account.urls')),
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
